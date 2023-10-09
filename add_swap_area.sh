f84d8644c1cb0bc8e4738ab6e8b7abd028bcd890
Rincahya Maragung <www.dewrin@gmail.com>
2023-10-10 01:16:57 +0700

    Update add_swap_area.sh

--- add_swap_area.sh
+++ add_swap_area.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-echo -e '\nIs adding swap area...\n'
+echo -e '\nChecking and adding swap area...\n'
 
-# Checking if 'swapfile' entry already exists in /etc/fstab
-grep -q "swapfile" /etc/fstab
-
-# Checking the exit code of the grep command
-if [[ ! $? -ne 0 ]]; then
-    # If swapfile entry already exists, print a message and skip to the next steps
-    echo -e '\nSwap file exists, skipping.\n'
+# Check if 'swapfile' exists
+if [ -e $HOME/swapfile ]; then
+    echo -e '\nSwap file already exists, skipping.\n'
 else
-    # If swapfile entry doesn't exist, start the process of creating a swapfile
+    # If swapfile doesn't exist, start the process of creating a swapfile in the home directory
     cd $HOME
 
-    # Allocating 8GB of space for the swapfile
+    # Allocate 8GB of space for the swapfile
     sudo fallocate -l 8G $HOME/swapfile
 
-    # Filling the swapfile with zeros using dd
+    # Fill the swapfile with zeros using dd
     sudo dd if=/dev/zero of=swapfile bs=1K count=8M
 
-    # Setting permissions for the swapfile
+    # Set permissions for the swapfile
     sudo chmod 600 $HOME/swapfile
 
-    # Creating swap on the swapfile
+    # Create swap on the swapfile
     sudo mkswap $HOME/swapfile
 
-    # Activating the swapfile
+    # Activate the swapfile
     sudo swapon $HOME/swapfile
 
-    # Displaying information about the swap
+    # Display information about the swap
     sudo swapon --show
 
-    # Adding an entry for the swapfile in /etc/fstab
-    echo $HOME'/swapfile swap swap defaults 0 0' >> /etc/fstab
+    # Add an entry for the swapfile in /etc/fstab
+    echo $HOME'/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
-    # Printing a completion message
-    echo -e '\nAdded successfully\n'
+    # Print a completion message
+    echo -e '\nSwap file added successfully.\n'
 fi
